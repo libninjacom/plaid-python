667ef92b7e68842595216f284fb004f1dc7b95ec
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-08-29 01:04:48 -0400

    try pdoc

--- plaid2/__init__.py
+++ plaid2/__init__.py
@@ -1,3 +1,8 @@
+"""
+This is a docstring for the whole package!
+
+*Created with [Libninja](https://www.libninja.com).*
+"""
 from .client import PlaidClient
 from .async_client import AsyncPlaidClient
 from .logger import logger, enable_debug_logging
